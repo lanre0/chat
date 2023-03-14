8af019cd9ba610760281e7874d9bcf3ed01af86c
lanre0 <43780262+lanre0@users.noreply.github.com>
2023-03-14 12:43:04 +0100

    spring cloud
    spring cloud added

--- src/main/java/com/example/chatdemo/ChatDemoApplication.java
+++ src/main/java/com/example/chatdemo/ChatDemoApplication.java
@@ -1,7 +1,9 @@
 package com.example.chatdemo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class ChatDemoApplication {
@@ -9,5 +11,7 @@ public class ChatDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ChatDemoApplication.class, args);
 	}
+
+
 
 }
